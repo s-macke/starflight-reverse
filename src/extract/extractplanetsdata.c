b31bfee67a85831d0e66c1499adf275cccf2b7ac
Sebastian Macke <sebastian@macke.de>
2019-02-09 22:59:16 +0100

    Add a few more options to extractplanetsdata

--- src/extract/extractplanetsdata.c
+++ src/extract/extractplanetsdata.c
@@ -25,6 +25,81 @@ void Continue()
     }
 }
 
+void GetAtmosphere(int idx)
+{
+  char str[1024];
+
+  FillKeyboardBufferString("ATMO @ .\n");
+  Continue();
+
+  //FillKeyboardBufferString("PLSET-OV ATMO.ACTIVITY @ ATMO.DENSITY C@\n");
+  FillKeyboardBufferString("PLSET-OV SURFTYPE C@ ATMO @ ATMO.DENSITY C@ ATMO.ACTIVITY @\n");
+  Continue();
+  printf("planet %i x=%3i y=%3i orbit=%i surftype=%i atmo=%2i atmospheric_density=%2i atmospheric_activity=%2i\n",
+    idx,
+    planets[idx].x,
+    planets[idx].y,
+    planets[idx].orbit,
+    Read16(regsp+8),
+    Read16(regsp+6),
+    Read16(regsp+4),
+    Read16(regsp+2)
+  );
+  Pop();
+  Pop();
+  Pop();
+  Pop();
+}
+
+void GetAtmosphericActivity()
+{
+  int histogramsky[20];
+  int histogramstorm[20];
+  memset(histogramsky, 0, sizeof(histogramsky));
+  memset(histogramstorm, 0, sizeof(histogramstorm));
+  FillKeyboardBufferString("STORM-OV\n");
+  Continue();
+  for(int i=0; i<10000; i++)
+  {
+    FillKeyboardBufferString("4 WF46C STORM @ #STORM @\n");
+    Continue();
+    histogramsky[Read16(regsp+2)]++;
+    histogramstorm[Read16(regsp+4)]++;
+    Pop();
+    Pop();
+  }
+  for(int i=0; i<13; i++)
+  {
+    printf("%-2i %-3i %-3i\n", i, histogramsky[i], histogramstorm[i]);
+  }
+}
+
+void GetScienceAndSensorReading(int idx)
+{
+  char str[1024];
+  printf("=== SCIENCE READING ===\n");
+  FillKeyboardBufferString("SCI-OV 32 .READINGS\n");
+  Continue();
+
+  sprintf(str, "%i %i SENSE-ADDR 1.5!\n",
+    planets[idx].instanceoffset&0xFFFF, planets[idx].instanceoffset>>16
+  );
+  FillKeyboardBufferString(str);
+  Continue();
+
+  FillKeyboardBufferString("AN-OV\n");
+  Continue();
+
+  // Patch science level
+  Write16(0xedc9, 0x0f30-2); // 1
+  Write16(0xedc9+2, 0x1692-2); // EXIT
+
+  printf("=== SENSOR READING ===\n");
+  FillKeyboardBufferString("32 (/ANALYSIS)\n");
+  Continue();
+
+}
+
 int main()
 {
   InitCPU();
@@ -47,12 +122,12 @@ int main()
   {
       if (planets[idx].species == -1) return 0;
 
-      printf("=== Generate planet %i x=%3i y=%3i orbit=%i offset=0x%06x ===\n",
+      printf("=== Generate planet %i x=%3i y=%3i orbit=%i species=%i ===\n",
         idx,
         planets[idx].x,
         planets[idx].y,
         planets[idx].orbit,
-        planets[idx].instanceoffset
+        planets[idx].species
       );
 
     char str[1024];
@@ -94,45 +169,47 @@ int main()
 /*
     FillKeyboardBufferString("(ORBIT) @>C+S INST-QTY @ . ICLOSE\n");
     Continue();
-*/
-/*
+
+    still needs, because it doesn't work
     // random generator if (ORBIT)=0
     FillKeyboardBufferString("PLSET-OV (PLANET) @>C+S WEC62\n");
     Continue();
 */
 
-    FillKeyboardBufferString("PLSET-OV\n");
-    Continue();
-
-    //if (idx == 235) EnableDebug();
-    FillKeyboardBufferString("PLAN-RUL\n");
-    Continue();
-    /*
     FillKeyboardBufferString("SET-PLANET\n");
     Continue();
+/*
+    GetAtmosphericActivity();
+    exit(1);
+*/
+    //GetAtmosphere(idx);
+    GetScienceAndSensorReading(idx);
+
+/*
+    FillKeyboardBufferString("MAPS- WF4F4\n");
+    Continue();
+    exit(1);
     */
 
-    printf("=== SCIENCE READING ===\n");
-    FillKeyboardBufferString("SCI-OV 32 .READINGS\n");
+
+/*
+    FillKeyboardBufferString("LP-OV\n");
     Continue();
-
-    sprintf(str, "%i %i SENSE-ADDR 1.5!\n",
-      planets[idx].instanceoffset&0xFFFF, planets[idx].instanceoffset>>16
-    );
-    FillKeyboardBufferString(str);
+    */
+/*
+    Write16(0xef31, 0x1692-2);
+    Write16(0xf0b0, 0x1692-2);
+*/
+    //EnableDebug();
+    /*
+    FillKeyboardBufferString("42 SPEC.PARM .\n");
     Continue();
-
-    FillKeyboardBufferString("AN-OV\n");
+    */
+    /*
+    FillKeyboardBufferString("42 BIO .\n");
     Continue();
-
-    // Patch science level
-    Write16(0xedc9, 0x0f30-2); // 1
-    Write16(0xedc9+2, 0x1692-2); // EXIT
-
-    printf("=== SENSOR READING ===\n");
-    FillKeyboardBufferString("32 (/ANALYSIS)\n");
-    Continue();
-
+    exit(1);
+    */
     idx++;
   }
   return 0;
