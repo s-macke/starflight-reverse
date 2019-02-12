bb55e3a49c033a02e0fb527c0d58c2c252149471
Sebastian Macke <sebastian@macke.de>
2019-02-12 23:25:25 +0100

    Update

---
+++ starflt2-out/data/strings.h
@@ -0,0 +1,151 @@
+typedef struct { int offset; char* string } STRINGENTRY;
+
+STRINGENTRY strings[]=
+{
+  { .offset=0x02468e, .string="NORTH"}
+  { .offset=0x0202f4, .string="IN OUR TYPICALLY CLEVER FASHION WE HAVE SOLD IT TO THE FOOLISH G'NUNK WHO WERE WILLING TO PAY AN EXORBITANT AMT. PRAISE THE GREAT BLOB GODDESS."}
+  { .offset=0x020281, .string=" UPON CLOSER EXAMINATION WE, THE HIGH BLOB COUNCIL, HAVE DETERMINED THAT THE MOST VALUABLE THING IS ACTUALLY JUST A WORTHLESS LUMP OF SHINEY GREEN GLASS."}
+  { .offset=0x020215, .string="NOTICE TO ALL SPEMIN WORSHIPPERS WHO HAVE COME HERE TO THESE SACRED LEGHK RUINS TO GAZE UPON THE MOST VALUABLE THING"}
+  { .offset=0x0201a4, .string="... LAST STRONGHOLD AT THE HALLS OF MEMORY, LOCATED AT 26S X 135E ... PREPARING FOR DEATH ... HOPE ... DESPAIR ... "}
+  { .offset=0x0200ee, .string="... NOT LONG BEFORE ... LAST OF US WILL BE FORCED TO CHOOSE BETWEEN SUICIDE OR PSYCHIC DOMINATION... WE ARE LOST... GAVE KEY TO THE GUARDIAN SATELLITE TO THE DWEENLE... PERHAPS SOME DAY, IF ANY OF US HAVE SURVIVED... REBUILD OUR CIVILIZATION."}
+  { .offset=0x02005c, .string="... HAVE DISCOVERED A CONTINUUM FLUX PATH THAT LEADS FROM THE HALLS OF MEMORY TO THE ANOMALY... FEW HAVE ATTEMPTED ESCAPE THROUGH THE ANOMALY. UNKNOWN IF... HAVE BEEN SUCCESSFUL."}
+  { .offset=0x020000, .string="... ONLY SYSTEM IN THE NEBULA... HALLS OF MEMORY... VITAL THAT IT REMAIN PROTECTED..."}
+  { .offset=0x01ff65, .string="THEY MIGHT POSSIBLY DESTROY IT OR USE IT FOR DESTRUCTIVE PURPOSES... HAVE CREATED THE GUARDIAN FORTRESS SATELLITE..."}
+  { .offset=0x01fee8, .string="..TO PROTECT THE HALLS OF MEMORY FROM THOSE UNDER THE CONTROL OF THE UHL... CANNOT AFFORD TO LET THEM HAVE ACCESS TO ALL OF THE INFORMATION THERE..."}
+  { .offset=0x01fe4d, .string="...BUT IT SEEMS TO BE COMPLETELY INVULNERABLE...FOR SOME REASON THE DWEENLE SEEM UNAFFECTED...DEATH TOLL IS NOW AT 16.4 BILLION AND PROSPECTS FOR THE FEW OF US THAT REMAIN SANE ARE NOT ENCOURAGING."}
+  { .offset=0x01fdcb, .string="...BREAKDOWN OF OUR ENTIRE CIVILIZATION...PANIC...MASS SUICIDES...HAVE ATTEMPTED TO FLEE IN COLONY SHIPS BUT THERE SIMPLY HASN'T BEEN ENOUGH TIME..."}
+  { .offset=0x01fd4b, .string="...CONTACT OF A VERY LIMITED NATURE. IT REFERS TO ITSELF AS UHL. IT APPEARS TO BE MUCH TOO STRONG FOR US. THERE IS LITTLE WE CAN DO. RECOMMEND THAT YOU..."}
+  { .offset=0x01fc9c, .string="...UNABLE TO DETERMINE ITS ORIGIN, ITS MOTIVATIONS, OR ITS PRECISE NATURE. WE DO NOT EVEN KNOW IF IT IS SENTIENT. ALL WE KNOW IS THAT IT PERVADES OUR SPACE AND THREATENS OUR EXISTENCE. MILLIONS HAVE GONE INSANE. PLEASE, WE NEED ASSISTANCE..."}
+  { .offset=0x01fc00, .string="THIS IS THE A-CLASS STAR THAT FORMS THE TRIANGLE. OF COURSE, I SUPPOSE IF THERE HAPPENED TO BE ... NO, THE COINCIDENCE WOULD BE TOO GREAT. YET, IT IS POSSIBLE. AND, IT WOULD EXPLAIN THIS PUZZLE... - CAPTAIN OF THE ELTHAR-ESH"}
+  { .offset=0x01fb61, .string="9 DAYS WE HAVE SPENT HERE TRYING TO FIGURE OUT WHERE WE COULD HAVE GONE WRONG. I AM CERTAIN THAT THOSE ON ELTHESH COULD NOT HAVE BEEN MISTAKEN. THE HALLS OF MEMORY SHOULD BE HERE."}
+  { .offset=0x01fae6, .string="IF WE ARE UNSUCCESFUL, THEN IT WILL BE TOO LATE FOR OUR RACE. WHOEVER YOU ARE, IF YOU ARE NOT UHL, GOOD LUCK AND WISH US LUCK IF YOU CAN. - CAPTAIN OF THE ELTHAR-ESH."}
+  { .offset=0x01fa4d, .string="WE SHALL LEAVE THE ORIGINAL HERE IN CASE WE ARE UNSUCCESFUL AND ANY OTHER ATTEMPTS ARE MADE AFTER US. NOW, WE SHALL PROCEED ON TO THE HALLS OF MEMORY, AND HOPEFULLY THIS TIME WE SHALL BE ABLE TO GET PAST THE GUARDIAN SATELLITE."}
+  { .offset=0x01f9b6, .string="TO ANY WHO MAY COME AFTER US. WE HAVE BUILT THIS STRUCTURE AND STOPPED HERE TO REST AND REPAIR OUR SHIP. WE HAVE STUDIED THE GUARDIAN TRANSMITTER KEY AND HAVE MADE A DUPLICATE FOR OUR OWN NEEDS."}
+  { .offset=0x01f93a, .string="WE SURMISE THAT THESE WERE CONSTELLATIONS IN THE LEGHK COSMOLOGY, BUT AS TO WHERE THESE ARE ... PERHAPS THOSE ABOARD THE ELTHAR-ESH WILL BE ABLE TO ANSWER THE QUESTION ... "}
+  { .offset=0x01f8c1, .string="WE HAVE ASCERTAINED THAT THE STAR SYSTEM THAT THE HALLS OF MEMORY ARE IN IS A GREEN CLASS-A STAR AND FORMS AN EQUILATERAL TRIANGLE WITH THE HOOK AND THE 6 YELLOW STONES."}
+  { .offset=0x01f877, .string="WE KNOW THAT THE HALLS ARE LOCATED SOMEWHERE NEAR THE COORDINATES 30S X 134E."}
+  { .offset=0x01f800, .string="IF ONLY WE COULD FIND IT, PERHAPS WE COULD FIND ANSWERS THERE THAT MIGHT SAVE US. WE KNOW THAT THEIR PLASMA BOLT WEAPONRY AND SHIELDING WERE FAR SUPERIOR TO OURS."}
+  { .offset=0x01f753, .string="... HAVE DISCOVERED THAT THERE EXISTS A PLACE BUILT BY THE LEGHK, AND REFERRED TO THEM AS THE HALLS OF MEMORY. THIS, WE HAVE DETERMINED, WAS THE REPOSITORY OF ALL THE CUMULATIVE LEARNING OF THE ANCIENT LEGHK CIVILIZATION."}
+  { .offset=0x01f701, .string="TAKE NOTE- BEWARE THE SHYNEUM PLANET IN THE SYSTEM 117,153. THERE RESIDES AN AWFUL THING."}
+  { .offset=0x01f64d, .string="TO ANY WHO FOLLOW- THIS IS TO STATE THAT WE BEAR OUR UMANU BROTHERS NO MALICE, FOR WE KNOW THAT IT WAS NOT REALLY THEY WHO DESTROYED US. OUR DEATHS DO NOT MATTER, AS ALL MUST DIE, BUT THE SADNESS OF THE NATURE OF OUR PARTING IS TOO MUCH FOR US TO BEAR."}
+  { .offset=0x024a04, .string="SNOW AND ICE"}
+  { .offset=0x024a1c, .string="BLACK AND STAR-FILLED."}
+  { .offset=0x024a3e, .string="CRATERED ROCK AND DUST"}
+  { .offset=0x024a60, .string="SAND"}
+  { .offset=0x024a70, .string="ROCK AND DIRT"}
+  { .offset=0x024a89, .string="CRACKED PLAINS AND HILLS OF"}
+  { .offset=0x024ab0, .string="VOLCANIC ROCK, STEAMING VENTS,"}
+  { .offset=0x024ada, .string="BOILING POOLS AND FLOWING LAVA"}
+  { .offset=0x024b04, .string="ROCKY SAND"}
+  { .offset=0x024b1a, .string="PEBBLY SAND"}
+  { .offset=0x024b31, .string="POWDERY SAND"}
+  { .offset=0x024b49, .string="COARSE SAND"}
+  { .offset=0x024b60, .string="OCEANS AND "}
+  { .offset=0x024b77, .string="SMOOTH AND FEATURELESS"}
+  { .offset=0x024b99, .string="DARK GREY"}
+  { .offset=0x024bae, .string="BLUE TINTED"}
+  { .offset=0x024bc5, .string="LIGHT GREY"}
+  { .offset=0x024bdb, .string="GREEN TINTED"}
+  { .offset=0x024c00, .string="YELLOWISH"}
+  { .offset=0x024c15, .string="RED TINTED"}
+  { .offset=0x024c2b, .string="REDDISH"}
+  { .offset=0x024c3e, .string="YELLOW TINTED"}
+  { .offset=0x024c57, .string="BROWNISH"}
+  { .offset=0x024c6b, .string="MUDDY BROWN"}
+  { .offset=0x024c82, .string="BLACK"}
+  { .offset=0x024c93, .string="DARK BLUE"}
+  { .offset=0x024ca8, .string="WHITE"}
+  { .offset=0x024cb9, .string="ORANGEISH"}
+  { .offset=0x024cce, .string="PURPLEISH"}
+  { .offset=0x024ce3, .string="GREENISH"}
+  { .offset=0x024cf7, .string="BLUEISH"}
+  { .offset=0x024d0a, .string="MOSS-LIKE"}
+  { .offset=0x024d1f, .string="GRASS-LIKE"}
+  { .offset=0x024d35, .string="FUNGOID"}
+  { .offset=0x024d48, .string="LICHEN-LIKE"}
+  { .offset=0x024d5f, .string="GELATINOUS"}
+  { .offset=0x024d75, .string="FIBROUS"}
+  { .offset=0x024d88, .string="LEAFY"}
+  { .offset=0x024d99, .string="SPONGY"}
+  { .offset=0x024dab, .string="COVERED IN PLACES BY A"}
+  { .offset=0x024dcd, .string="CARPET LIFEFORM"}
+  { .offset=0x024de8, .string="WITH "}
+  { .offset=0x024df9, .string="IT IS RAINING HEAVILY."}
+  { .offset=0x024e1b, .string="IT IS OVERCAST."}
+  { .offset=0x024e36, .string="IT IS SNOWING LIGHTLY."}
+  { .offset=0x024e58, .string="IT IS MISTY."}
+  { .offset=0x024e70, .string="YOU ARE IN AN ELECTRICAL STORM."}
+  { .offset=0x024e9b, .string="THE GROUND IS SHAKING AND RUMBLING."}
+  { .offset=0x024eca, .string="IT IS SNOWING HEAVILY."}
+  { .offset=0x024eec, .string="IT IS HAILING."}
+  { .offset=0x024f06, .string="YOU ARE IN A THUNDER STORM."}
+  { .offset=0x024f2d, .string="YOU ARE IN A WINDSTORM."}
+  { .offset=0x024f50, .string="IT IS FOGGY."}
+  { .offset=0x024f68, .string="IT IS HAZY."}
+  { .offset=0x024f7f, .string="IT IS RAINING."}
+  { .offset=0x024f99, .string="IT IS CLEAR."}
+  { .offset=0x024fb1, .string="NITROGEN"}
+  { .offset=0x024fc5, .string="OXYGEN"}
+  { .offset=0x024fd7, .string="METHANE"}
+  { .offset=0x025000, .string="CARBON MONOXIDE"}
+  { .offset=0x02501b, .string="WATER"}
+  { .offset=0x02502c, .string="AMMONIA COMPOUNDS"}
+  { .offset=0x025049, .string="CHLORINE COMPOUNDS"}
+  { .offset=0x025067, .string="METHANOL, ETHANOL"}
+  { .offset=0x025084, .string="SULFUR COMPOUNDS"}
+  { .offset=0x0250a0, .string="SILICON COMPOUNDS"}
+  { .offset=0x0250bd, .string="SODIUM COMPOUNDS"}
+  { .offset=0x0250d9, .string="FLUORINE COMPOUNDS"}
+  { .offset=0x0250f7, .string="METAL COMPOUNDS"}
+  { .offset=0x025112, .string="HYDROGEN, HELIUM"}
+  { .offset=0x02512e, .string="NITROGEN, OXYGEN"}
+  { .offset=0x02514a, .string="OXYGEN, CARBON DIOXIDE"}
+  { .offset=0x02516c, .string="AMMONIA, HYDROGEN"}
+  { .offset=0x025189, .string="METHANE, AMMONIA, ARGON"}
+  { .offset=0x0251ac, .string="FLUORINE, CHLORINE"}
+  { .offset=0x0251ca, .string="METHANE, HYDROGEN CYANIDE"}
+  { .offset=0x0251ef, .string="OXYGEN, HYDROGEN"}
+  { .offset=0x02520b, .string="HYDROGEN"}
+  { .offset=0x02521f, .string="NONE"}
+  { .offset=0x0249f2, .string="BLACK"}
+  { .offset=0x0249e1, .string="GREY"}
+  { .offset=0x0249cf, .string="WHITE"}
+  { .offset=0x0249b5, .string="MULTI-COLORED"}
+  { .offset=0x0249a0, .string="LAVENDAR"}
+  { .offset=0x02498a, .string="TURQUOISE"}
+  { .offset=0x024973, .string="LIME GREEN"}
+  { .offset=0x024958, .string="BROWNISH GREEN"}
+  { .offset=0x02493d, .string="COPPER-COLORED"}
+  { .offset=0x02492b, .string="BEIGE"}
+  { .offset=0x024919, .string="BROWN"}
+  { .offset=0x024908, .string="PINK"}
+  { .offset=0x0248ef, .string="RUST-COLORED"}
+  { .offset=0x0248db, .string="MARROON"}
+  { .offset=0x0248c8, .string="PURPLE"}
+  { .offset=0x0248b5, .string="VIOLET"}
+  { .offset=0x02489f, .string="VERY BLUE"}
+  { .offset=0x02488e, .string="BLUE"}
+  { .offset=0x024877, .string="BLUE-GREEN"}
+  { .offset=0x024865, .string="GREEN"}
+  { .offset=0x024849, .string="YELLOWISH GREEN"}
+  { .offset=0x024830, .string="LEMON YELLOW"}
+  { .offset=0x02481d, .string="YELLOW"}
+  { .offset=0x024800, .string="YELLOWISH ORANGE"}
+  { .offset=0x0247d9, .string="ORANGE"}
+  { .offset=0x0247be, .string="REDDISH ORANGE"}
+  { .offset=0x0247ae, .string="RED"}
+  { .offset=0x024796, .string="SUBFREEZING"}
+  { .offset=0x024782, .string="FURNACE"}
+  { .offset=0x02476e, .string="SEARING"}
+  { .offset=0x024759, .string="TROPICAL"}
+  { .offset=0x024743, .string="TEMPERATE"}
+  { .offset=0x024730, .string="ARCTIC"}
+  { .offset=0x024719, .string="NORTH WEST"}
+  { .offset=0x024708, .string="WEST"}
+  { .offset=0x0246f1, .string="SOUTH WEST"}
+  { .offset=0x0246df, .string="SOUTH"}
+  { .offset=0x0246c8, .string="SOUTH EAST"}
+  { .offset=0x0246b7, .string="EAST"}
+  { .offset=0x0246a0, .string="NORTH EAST"}
+  { .offset=-1, .string=NULL}
+};
